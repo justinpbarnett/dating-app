bcf32bdff794127c9be1c05f2f7dc900b5d28f70
Justin Barnett <justin@buildwithfoundry.com>
2023-12-28 15:42:05 -0500

    Initial Commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,46 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController
+{
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        return await _context.Users.ToListAsync();
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<AppUser>> GetUser(int id)
+    {
+        return await _context.Users.FindAsync(id);
+    }
+    
+    [HttpPost]
+    public async Task<ActionResult> CreateUser(AppUser user)
+    {
+        _context.Users.Add(user);
+        await _context.SaveChangesAsync();
+        return new OkResult();
+    }
+
+    [HttpPut]
+    public async Task<ActionResult> UpdateUser(AppUser user)
+    {
+        _context.Entry(user).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+        return new OkResult();
+    }
+}
